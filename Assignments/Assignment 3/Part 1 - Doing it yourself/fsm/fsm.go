0cb268dce625a455c15e172c1a2adf4577bd76cd
kritagya03 <kriagya03@gmail.com>
2026-01-25 13:14:53 +0100

    Assignment 3: Working single elevator

--- Assignments/Assignment 3/Part 1 - Doing it yourself/fsm/fsm.go
+++ Assignments/Assignment 3/Part 1 - Doing it yourself/fsm/fsm.go
@@ -10,11 +10,12 @@ import (
 // We need a channel to handle the door timeout, similar to the timer logic in C
 var doorTimer *time.Timer
 
-func init() {
-	// Initialize the timer but stop it immediately so it doesn't fire
-	doorTimer = time.NewTimer(time.Hour)
-	doorTimer.Stop()
-}
+// func init() {
+// 	fmt.Println("init: initialized one hour timer, but don't start it.")
+// 	// Initialize the timer but stop it immediately so it doesn't fire
+// 	doorTimer = time.NewTimer(time.Hour)
+// 	doorTimer.Stop()
+// }
 
 // SetAllLights sets the button lamps based on requests
 func SetAllLights(e elevator.Elevator) {
@@ -23,10 +24,12 @@ func SetAllLights(e elevator.Elevator) {
 			elevio.SetButtonLamp(elevio.ButtonType(btn), f, e.Requests[f][btn])
 		}
 	}
+	fmt.Println("SetAllLights: updated all lights from e.Requests.")
 }
 
 // OnInitializeBetweenFloors moves the elevator down if it starts between floors
 func OnInitializeBetweenFloors(e *elevator.Elevator) {
+	fmt.Println("OnInitializeBetweenFloors: wanting to move down.")
 	elevio.SetMotorDirection(elevio.MD_Down)
 	e.Direction = elevio.MD_Down
 	e.Behaviour = elevator.EB_Moving
@@ -40,33 +43,42 @@ func OnRequestButtonPress(btnEvent elevio.ButtonEvent, e *elevator.Elevator, doo
 	switch e.Behaviour {
 	case elevator.EB_DoorOpen:
 		if e.ShouldClearImmediately(btnEvent.Floor, btnEvent.ButtonCallType) {
+			fmt.Println("OnRequestButtonPress - EB_DoorOpen - ShouldClearImmediately=true: Wanting to start the timer again.")
 			// Reset the timer
 			doorTimerReset <- true
 		} else {
 			e.Requests[btnEvent.Floor][btnEvent.ButtonCallType] = true
+			fmt.Println("OnRequestButtonPress - EB_DoorOpen - ShouldClearImmediately=false: Added order to e.Requests.")
 		}
 
 	case elevator.EB_Moving:
 		e.Requests[btnEvent.Floor][btnEvent.ButtonCallType] = true
+		fmt.Println("OnRequestButtonPress - EB_Moving: Added order to e.Requests.")
 
 	case elevator.EB_Idle:
 		e.Requests[btnEvent.Floor][btnEvent.ButtonCallType] = true
-		pair := e.ChooseDirection()
-		e.Direction = pair.Direction
-		e.Behaviour = pair.Behaviour
+		fmt.Println("OnRequestButtonPress - EB_Idle: Added order to e.Requests.")
+		directionBehaviourPair := e.ChooseDirection()
+		e.Direction = directionBehaviourPair.Direction
+		e.Behaviour = directionBehaviourPair.Behaviour
+		fmt.Println("OnRequestButtonPress - EB_Idle: Chosen new direction:", directionBehaviourPair)
 
-		switch pair.Behaviour {
+		switch directionBehaviourPair.Behaviour {
 		case elevator.EB_DoorOpen:
 			elevio.SetDoorOpenLamp(true)
 			doorTimerReset <- true
 			e.ClearAtCurrentFloor()
+			fmt.Println("OnRequestButtonPress - EB_Idle - New behaviour is EB_DoorOpen: Opened door, wanting to start the timer, maybe remove order(s) at floor if possible.")
 		case elevator.EB_Moving:
 			elevio.SetMotorDirection(e.Direction)
+			fmt.Println("OnRequestButtonPress - EB_Idle - New behaviour is EB_Moving: set the motor direction: ", e.Direction)
 		case elevator.EB_Idle:
 			// Do nothing
+			fmt.Println("OnRequestButtonPress - EB_Idle - New behaviour is EB_Idle: Doing nothing")
 		}
 	}
 
+	fmt.Println("OnRequestButtonPress: Wanting to update all lights.")
 	SetAllLights(*e)
 	fmt.Println("\nNew state:")
 	e.Print()
@@ -77,12 +89,14 @@ func OnFloorArrival(newFloor int, e *elevator.Elevator, doorTimerReset chan<- bo
 	fmt.Printf("\n\n%s(%d)\n", "OnFloorArrival", newFloor)
 	e.Print()
 
+	fmt.Println("OnFloorArrival: wanting to set floor indicator.")
 	e.Floor = newFloor
 	elevio.SetFloorIndicator(e.Floor)
 
 	switch e.Behaviour {
 	case elevator.EB_Moving:
 		if e.ShouldStop() {
+			fmt.Println("OnFloorArrival - EB_Moving - e.ShouldStop()==True: wanting to stop, open door, maybe clear order(s) at floor, reset timer, update lights.")
 			elevio.SetMotorDirection(elevio.MD_Stop)
 			elevio.SetDoorOpenLamp(true)
 			e.ClearAtCurrentFloor()
@@ -91,6 +105,8 @@ func OnFloorArrival(newFloor int, e *elevator.Elevator, doorTimerReset chan<- bo
 			e.Behaviour = elevator.EB_DoorOpen
 		}
 	default:
+		// Can enter here if initializing elevator on a floor
+		fmt.Printf("OnFloorArrival - e.Behaviour==default(%v): Doing nothing.\n", e.Behaviour)
 		// Should not happen if strictly following FSM, but safe to ignore
 	}
 
@@ -105,21 +121,26 @@ func OnDoorTimeout(e *elevator.Elevator, doorTimerReset chan<- bool) {
 
 	switch e.Behaviour {
 	case elevator.EB_DoorOpen:
-		pair := e.ChooseDirection()
-		e.Direction = pair.Direction
-		e.Behaviour = pair.Behaviour
+		fmt.Println("OnDoorTimeout - EB_DoorOpen: choosing new direction.")
+		directionBehaviourPair := e.ChooseDirection()
+		e.Direction = directionBehaviourPair.Direction
+		e.Behaviour = directionBehaviourPair.Behaviour
 
 		switch e.Behaviour {
 		case elevator.EB_DoorOpen:
+			fmt.Println("OnDoorTimeout - EB_DoorOpen - new behaviour is EB_DoorOpen: wanting to reset timer, maybe clear order(s) at floor, updating lights.")
 			doorTimerReset <- true
 			e.ClearAtCurrentFloor()
 			SetAllLights(*e)
 		case elevator.EB_Moving, elevator.EB_Idle:
+			fmt.Printf("OnDoorTimeout - EB_DoorOpen - new behaviour is %v: opening door, setting direction %v.\n", e.Behaviour, e.Direction)
 			elevio.SetDoorOpenLamp(false)
 			elevio.SetMotorDirection(e.Direction)
 		}
 
 	default:
+		// If initializing the elevator on a floor, this happens
+		fmt.Printf("OnDoorTimeout - e.Behaviour==default(%v): Doing nothing.\n", e.Behaviour)
 		// Should not happen
 	}
 
--- Assignments/Assignment 3/Part 1 - Doing it yourself/main.go
+++ Assignments/Assignment 3/Part 1 - Doing it yourself/main.go
@@ -26,8 +26,10 @@ func main() {
 	// 3. Check if we need to move to a floor first (Startup logic)
 	// Note: elevio.GetFloor() returns -1 if between floors
 	if floor := elevio.GetFloor(); floor == -1 {
+		fmt.Println("Elevator started between two floors.")
 		fsm.OnInitializeBetweenFloors(&elev)
 	} else {
+		fmt.Printf("Elevator started at a floor==%d\n", floor)
 		elev.Floor = floor
 	}
 
@@ -46,8 +48,8 @@ func main() {
 	// We use a timer that we can reset.
 	// We use a buffered channel to trigger the timer reset to avoid blocking in FSM functions
 	doorTimeout := time.NewTimer(time.Duration(elev.Config.DoorOpenDurationS) * time.Second)
-	doorTimeout.Stop() // Don't start immediately
-	doorTimerReset := make(chan bool)
+	doorTimeout.Stop()                    // Don't start immediately
+	doorTimerReset := make(chan bool, 10) // Make the channel buffered to avoid deadlock (not block)
 
 	fmt.Println("Elevator started!")
 
@@ -55,54 +57,69 @@ func main() {
 		select {
 		// Handle Button Press
 		case btnEvent := <-drv_buttons:
+			fmt.Println("<-drv_buttons")
 			fsm.OnRequestButtonPress(btnEvent, &elev, doorTimerReset)
 
 		// Handle Floor Arrival
 		case newFloor := <-drv_floors:
+			fmt.Println("<-drv_floors")
 			fsm.OnFloorArrival(newFloor, &elev, doorTimerReset)
 
 		// Handle Door Timeout
 		case <-doorTimeout.C:
+			fmt.Println("<-doorTimeout.C")
 			fsm.OnDoorTimeout(&elev, doorTimerReset)
 
 		// Handle Timer Reset
 		case <-doorTimerReset:
+			fmt.Println("<-doorTimerReset")
 			// Drain the channel if it has expired but not yet received
 			if !doorTimeout.Stop() {
+				fmt.Println("doorTimerReset handler: Trying to stop an already expired timer.")
 				select {
 				case <-doorTimeout.C:
+					fmt.Println("doorTimerReset handler: Removing one message from the doorTimeout channel.")
 				default:
 				}
 			}
 			doorTimeout.Reset(time.Duration(elev.Config.DoorOpenDurationS) * time.Second)
+			fmt.Println("doorTimerReset handler: Timer has started again from 0.")
 
 		// Handle Obstruction (Optional/Extra)
 		case obstr := <-drv_obstr:
-			// fmt.Printf("Obstruction: %v\n", obstr)
-			// // Implementation depends on specific requirements, often pauses the door timer
-			// if obstr && elev.Behaviour == elevator.EB_DoorOpen {
-			// 	elevio.SetDoorOpenLamp(true) // Ensure it's on
-			// 	if !doorTimeout.Stop() {
-			// 		select {
-			// 		case <-doorTimeout.C:
-			// 		default:
-			// 		}
-			// 	}
-			// 	// Timer is effectively paused (stopped).
-			// 	// Logic to resume when obstruction clears would go here.
-			// } else if !obstr && elev.Behaviour == elevator.EB_DoorOpen {
-			// 	// Resume timer
-			// 	doorTimeout.Reset(time.Duration(elev.Config.DoorOpenDurationS) * time.Second)
-			// }
+			fmt.Println("<-drv_obstr")
+			fmt.Printf("Obstruction: %v\n", obstr)
+			// Implementation depends on specific requirements, often pauses the door timer
+			if obstr && elev.Behaviour == elevator.EB_DoorOpen {
+				elevio.SetDoorOpenLamp(true) // Ensure it's on
+				fmt.Println("obstr handler - obstr==true - EB_DoorOpen: Ensuring door is open.")
+				if !doorTimeout.Stop() {
+					fmt.Println("obstr handler - obstr==true - EB_DoorOpen: Trying to stop an already expired timer.")
+					select {
+					case <-doorTimeout.C:
+						fmt.Println("obstr handler: Removing one message from the doorTimeout channel.")
+					default:
+					}
+				}
+				// Timer is effectively paused (stopped).
+				fmt.Println("obstr handler: Logic to resume when obstruction clears would go here.")
+				// Logic to resume when obstruction clears would go here. TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			} else if !obstr && elev.Behaviour == elevator.EB_DoorOpen {
+				// Resume timer
+				doorTimeout.Reset(time.Duration(elev.Config.DoorOpenDurationS) * time.Second)
+				fmt.Println("obstr handler - obstr==false - EB_DoorOpen: Timer has started again from 0.")
+			}
 
 		// Handle Stop Button (Optional/Extra)
 		case stop := <-drv_stop:
-			// fmt.Printf("Stop button: %v\n", stop)
-			// for f := 0; f < numFloors; f++ {
-			// 	for b := elevio.ButtonType(0); b < 3; b++ {
-			// 		elevio.SetButtonLamp(b, f, false)
-			// 	}
-			// }
+			fmt.Println("<-drv_stop")
+			fmt.Printf("Stop button: %v\n", stop)
+			for f := 0; f < numFloors; f++ {
+				for b := elevio.ButtonType(0); b < 3; b++ {
+					elevio.SetButtonLamp(b, f, false)
+				}
+			}
+			fmt.Println("stop handler: Cleared all lights. IT JUST TURNS OFF THE LIGHTS. IT DOESN'T REMOVE THE ORDER FROM THE ELEVATOR REQUESTS 2D ARRAY.")
 		}
 	}
 }
